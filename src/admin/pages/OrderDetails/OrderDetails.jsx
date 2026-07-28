43423fe8ca5c66b5bd2c810d9d03318afab177a2
Yogesh Pal <159111601+theyogeshpal@users.noreply.github.com>
2026-07-28 23:22:31 +0530

    Fix api import in OrderDetails

--- src/admin/pages/OrderDetails/OrderDetails.jsx
+++ src/admin/pages/OrderDetails/OrderDetails.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
-import api from '../../../utils/api';
+import api from '../../config/api';
 import Swal from 'sweetalert2';
 import './OrderDetails.css';
 
