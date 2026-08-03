55cb301e0ad42f689f8cc951444f822c095426cc
Yogesh Pal <159111601+theyogeshpal@users.noreply.github.com>
2026-08-04 00:04:16 +0530

    Add vite plugin legacy for older device support

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import legacy from '@vitejs/plugin-legacy'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
+    legacy({
+      targets: ['defaults', 'not IE 11']
+    })
   ],
 })
